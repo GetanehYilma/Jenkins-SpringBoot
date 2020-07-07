5f563afe115dc705e0f47f9b49cd34fab201db49
GetanehYilma <gyletike@mum.edu>
2020-07-06 21:54:53 -0500

    jenkins first

---
+++ src/main/java/com/getaneh/jenkinssb/HomeController.java
@@ -0,0 +1,15 @@
+package com.getaneh.jenkinssb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(){
+
+        return "homePage";
+    }
+}
